c8efea1b2563d52146ebe871ab89ceb3a8f10ccd
Julio-Pupim <julio.minellamatheus@gmail.com>
2023-11-03 14:57:26 -0300

    feat: pessoa e contato mapeados

--- DesafioElotech/src/main/java/org/elotech/desafio/model/Contato.java
+++ DesafioElotech/src/main/java/org/elotech/desafio/model/Contato.java
@@ -1,5 +1,42 @@
 package org.elotech.desafio.model;
 
-public class Contato {
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="contato")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contato implements Serializable{
+	
+	private static final long serialVersionUID = 8681104137432506922L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "contato_idkey_seq")
+    @SequenceGenerator(name = "contato_idkey_seq", sequenceName = "contato_idkey_seq", allocationSize = 1)
+	@Column(name="idkey")
+	private Long id;
+	
+	private String nome;
+	
+	private String telefone;
+	
+	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "idkey_pessoa",referencedColumnName = "idkey")
+	private Pessoa pessoa;
 }
--- DesafioElotech/src/main/java/org/elotech/desafio/model/Pessoa.java
+++ DesafioElotech/src/main/java/org/elotech/desafio/model/Pessoa.java
@@ -1,5 +1,45 @@
 package org.elotech.desafio.model;
 
-public class Pessoa {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "pessoa")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pessoa implements Serializable{
+	
+	private static final long serialVersionUID = 5779762933177727652L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pessoa_idkey_seq")
+    @SequenceGenerator(name = "pessoa_idkey_seq", sequenceName = "pessoa_idkey_seq",allocationSize = 1)
+	@Column(name = "idkey")
+	private Long id;
+	
+	private String nome;
+	
+	private String cpf;
+	
+	@Column(name="datanascimento")
+	private LocalDate dataNascimento;
+	
+	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Contato> contatosList = new ArrayList<>();
+	
 }
