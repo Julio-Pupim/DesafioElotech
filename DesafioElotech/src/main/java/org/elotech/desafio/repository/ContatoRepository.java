dc5acdc9d688f64140acfda6cb061fc3911a838d
Julio-Pupim <julio.minellamatheus@gmail.com>
2023-11-06 13:48:51 -0300

    fix: adding filter by name on repository

--- DesafioElotech/src/main/java/org/elotech/desafio/repository/ContatoRepository.java
+++ DesafioElotech/src/main/java/org/elotech/desafio/repository/ContatoRepository.java
@@ -1,8 +1,12 @@
 package org.elotech.desafio.repository;
 
 import org.elotech.desafio.model.Contato;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContatoRepository extends JpaRepository<Contato, Long>{
 
+public interface ContatoRepository extends JpaRepository<Contato, Long>{
+	
+	Page<Contato> findByNome(String nome, Pageable pageable);
 }
--- DesafioElotech/src/main/java/org/elotech/desafio/repository/PessoaRepository.java
+++ DesafioElotech/src/main/java/org/elotech/desafio/repository/PessoaRepository.java
@@ -1,8 +1,12 @@
 package org.elotech.desafio.repository;
 
 import org.elotech.desafio.model.Pessoa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+	
+	Page<Pessoa> findByNome(String nome, Pageable pageable);
 }
