82577ddd7fdf4ce56fdf315fea1faecfebd71402
Julio-Pupim <julio.minellamatheus@gmail.com>
2023-11-06 20:06:54 -0300

    fix: add imports

--- DesafioElotech/src/test/java/org/elotech/desafio/services/ContatoServiceTest.java
+++ DesafioElotech/src/test/java/org/elotech/desafio/services/ContatoServiceTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.elotech.desafio.repository.ContatoRepository;
 import org.elotech.desafio.service.ContatoService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
