f0a264395512cf91f4d031a188133296bea35679
Julio-Pupim <julio.minellamatheus@gmail.com>
2023-11-06 13:47:30 -0300

    feat: created recordsDTO

--- DesafioElotech/src/main/java/org/elotech/desafio/dto/ContatoRecordDTO.java
+++ DesafioElotech/src/main/java/org/elotech/desafio/dto/ContatoRecordDTO.java
@@ -1,5 +1,11 @@
 package org.elotech.desafio.dto;
 
-public record ContatoRecordDTO() {
+import org.elotech.desafio.model.Pessoa;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ContatoRecordDTO(@NotNull @NotBlank String nome, @NotNull @NotBlank String telefone, @NotNull @NotBlank String email, Pessoa pessoa) {
 
 }
+
--- DesafioElotech/src/main/java/org/elotech/desafio/dto/PessoaRecordDTO.java
+++ DesafioElotech/src/main/java/org/elotech/desafio/dto/PessoaRecordDTO.java
@@ -1,5 +1,15 @@
 package org.elotech.desafio.dto;
 
-public class PessoaRecordDTO {
+import java.time.LocalDate;
+import java.util.List;
+
+import org.elotech.desafio.model.Contato;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+public record PessoaRecordDTO(@NotNull @NotBlank String nome, @NotNull @NotBlank String cpf,@NotNull @Past LocalDate dataNascimento, @NotEmpty List<Contato> contatosList) {
 
 }
