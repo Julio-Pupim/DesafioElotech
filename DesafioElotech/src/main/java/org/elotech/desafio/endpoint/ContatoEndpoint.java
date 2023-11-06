a87c1c07de8d7b0f22e9bfdf984c638638b0ce0e
Julio-Pupim <julio.minellamatheus@gmail.com>
2023-11-06 13:47:58 -0300

    feat: created endpoints for pessoa and contato

--- DesafioElotech/src/main/java/org/elotech/desafio/endpoint/ContatoEndpoint.java
+++ DesafioElotech/src/main/java/org/elotech/desafio/endpoint/ContatoEndpoint.java
@@ -1,5 +1,95 @@
 package org.elotech.desafio.endpoint;
 
-public class ContatoEndpoint {
+import java.util.Optional;
 
+import org.elotech.desafio.dto.ContatoRecordDTO;
+import org.elotech.desafio.model.Contato;
+import org.elotech.desafio.repository.ContatoRepository;
+import org.elotech.desafio.service.ContatoService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/contatos")
+@AllArgsConstructor
+public class ContatoEndpoint {
+	
+	private final ContatoService service;
+	private final ContatoRepository repository;
+	
+	
+	@GetMapping
+	public ResponseEntity<Page<Contato>> buscarContatos(@RequestParam(defaultValue= "")String nome,
+			  											@RequestParam(defaultValue = "0") int page,
+			  											@RequestParam(defaultValue = "10") int size){
+		
+		Pageable pageable = PageRequest.of(page, size);
+		
+		Page<Contato> contatoPage= repository.findByNome(nome, pageable);
+		
+		if(contatoPage.isEmpty()) {
+			
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(contatoPage);
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(contatoPage);
+	} 
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> buscarContato(@PathVariable(value = "id")Long id){
+		
+		Optional<Contato> contatoOptional = repository.findById(id);
+		
+		if(contatoOptional.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Contato não encontrado");
+
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(contatoOptional.get());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Contato> salvarContato(@RequestBody @Valid ContatoRecordDTO contatoRecord){
+		Contato contato = new Contato();
+		BeanUtils.copyProperties(contatoRecord, contato);
+		Contato contatoSalvo = service.salvarContato(contato);
+		return ResponseEntity.status(HttpStatus.CREATED).body(contatoSalvo);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> atualizarContato(@PathVariable(value="id")Long id, @RequestBody @Valid ContatoRecordDTO contatoRecord){
+		Optional<Contato> contatoOptional = repository.findById(id);
+		if(contatoOptional.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Contato não encontrado");
+		}
+		Contato contato = contatoOptional.get();
+		BeanUtils.copyProperties(contatoRecord,contato);
+		Contato contatoSalvo = service.salvarContato(contato);
+		return ResponseEntity.status(HttpStatus.OK).body(contatoSalvo);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deletarContato(@PathVariable(value="id")Long id){
+		Optional<Contato> contato = repository.findById(id);
+		if(contato.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Contato não encontrado");
+		}
+		repository.deleteById(id);
+		return ResponseEntity.status(HttpStatus.OK).body("Contato deletado");
+	}
 }
--- DesafioElotech/src/main/java/org/elotech/desafio/endpoint/PessoaEndpoint.java
+++ DesafioElotech/src/main/java/org/elotech/desafio/endpoint/PessoaEndpoint.java
@@ -1,5 +1,91 @@
 package org.elotech.desafio.endpoint;
 
-public class PessoaEndpoint {
+import java.util.Optional;
 
+import org.elotech.desafio.dto.PessoaRecordDTO;
+import org.elotech.desafio.model.Pessoa;
+import org.elotech.desafio.repository.PessoaRepository;
+import org.elotech.desafio.service.PessoaService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/pessoas")
+@AllArgsConstructor
+public class PessoaEndpoint {
+	
+	private final PessoaService service;
+	
+	private final PessoaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<Page<Pessoa>> buscarPessoas(@RequestParam(defaultValue= "")String nome,
+													  @RequestParam(defaultValue = "0") int page,
+													  @RequestParam(defaultValue = "10") int size){
+		Pageable pageable = PageRequest.of(page, size);
+		Page<Pessoa> pessoaPage= repository.findByNome(nome, pageable);
+		if(pessoaPage.isEmpty()) {
+			
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(pessoaPage);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(pessoaPage);
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> buscarUmaPessoa(@PathVariable(value = "id")Long id){
+		
+		Optional<Pessoa> pessoaOptional = repository.findById(id);
+		
+		if(pessoaOptional.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não encontrada");
+
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(pessoaOptional.get());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> salvarPessoa(@RequestBody @Valid PessoaRecordDTO pessoaRecord){
+		Pessoa pessoa = new Pessoa();
+		BeanUtils.copyProperties(pessoaRecord, pessoa);
+		Pessoa pessoaSalva = service.salvarPessoa(pessoa);
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> atualizarPessoa(@PathVariable(value="id")Long id, @RequestBody @Valid PessoaRecordDTO pessoaRecord){
+		Optional<Pessoa> pessoaOptional = repository.findById(id);
+		if(pessoaOptional.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não encontrada");
+		}
+		Pessoa pessoa = pessoaOptional.get();
+		BeanUtils.copyProperties(pessoaRecord,pessoa);
+		Pessoa pessoaSalva = service.salvarPessoa(pessoa);
+		return ResponseEntity.status(HttpStatus.OK).body(pessoaSalva);
+	}	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deletarPessoa(@PathVariable(value="id")Long id){
+		Optional<Pessoa> pessoa = repository.findById(id);
+		if(pessoa.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não encontada");
+		}
+		repository.deleteById(id);
+		return ResponseEntity.status(HttpStatus.OK).body("Pessoa deletada");
+	}
+	
 }
